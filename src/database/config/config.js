46341febb6d386964dca68699e2f3ebd6711e7ec
kaytbode <bkaoluborode@gmail.com>
2019-09-05 18:24:38 +0100

    chore: set up sequelize

---
+++ src/database/config/config.js
@@ -0,0 +1,29 @@
+
+const dbConfig = {
+  development: {
+    username: 'root',
+    password: null,
+    database: 'database_development',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'database_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'database_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    operatorsAliases: false,
+  },
+};
+
+export default dbConfig;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv';
+import db from './database/models/index'
+
+dotenv.config();
+
+console.log('hey');
+
+console.log(db);
